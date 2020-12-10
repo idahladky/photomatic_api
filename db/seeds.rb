20efe60063e13bab1ad21cd0a42c32c2d042558e
Ida <idahuladki@yahoo.com>
2020-12-09 20:29:26 -0500

    database seeded, cors set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Photo_post.create([
+PhotoPost.create([
     { image: "F63F7187-BEDF-489E-B6AC-DB3EBFD6B2F4.JPG", notes: "Beach Bungalow, Ocean Grove, NJ, June 2020", author: "idaleighwatson" },
     { image: "C9E7AE84-68A6-4723-A5F5-7A8F5DEBBCA7.JPG", notes: "a night for contemplation", author: "idaleighwatson" },
     { image: "F0CFDBA2-25B0-4FEA-89E4-9C04782425B8.JPG", notes: "That's Entertainment, Asbury Park, NJ, June 2020", author: "idaleighwatson" },
