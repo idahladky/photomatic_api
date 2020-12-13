485c01ef1d8cb65dff5fa71b9dc624b89ac235a2
Ida <idahuladki@yahoo.com>
2020-12-12 19:53:37 -0500

    paperclip gem, migration files, and models added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_011448) do
+ActiveRecord::Schema.define(version: 2020_12_12_234801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2020_12_11_011448) do
     t.string "image"
     t.string "notes"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
